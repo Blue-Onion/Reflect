205e450ac0debabf534f55d76fb6e0f27c8d28b8
Blue Onion <rawatadityasingh963@gmail.com>
2025-03-27 21:31:54 +0530

    Analtics and skeleton

--- app/(main)/dashboard/_components/MoodAnalaysis.jsx
+++ app/(main)/dashboard/_components/MoodAnalaysis.jsx
@@ -6,15 +6,33 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { getAnalytics } from "@/actions/analytics";
+import { useFetch } from "@/hooks/use-fetch";
+import { useUser } from "@clerk/nextjs";
+import MoodAnalyticsSkeleton from "./analytics-loading";
 const MoodAnalaysis = () => {
   const timeOption = [
     { value: "7d", label: "Last 7 days" },
     { value: "15d", label: "Last 15 days" },
     { value: "30d", label: "Last 30 days" },
   ];
+  const { isLoaded } = useUser();
+  const {
+    loading,
+    fn: fetchAnalytics,
+    data: analytics,
+  } = useFetch(getAnalytics);
   const [peroid, setperoid] = useState("7d");
+  useEffect(() => {
+    fetchAnalytics(peroid);
+  }, [peroid]);
+
+
+  console.log(analytics);
+if(loading||!isLoaded||!analytics?.data){
+return <MoodAnalyticsSkeleton/>
+}
   return (
     <>
       <div className="flex justify-between items-center">
