e48a76c19e964724e1cd08344604393ee033e7f3
Blue Onion <rawatadityasingh963@gmail.com>
2025-03-28 20:21:14 +0530

    Cards Done

--- app/(main)/dashboard/_components/MoodAnalaysis.jsx
+++ app/(main)/dashboard/_components/MoodAnalaysis.jsx
@@ -1,4 +1,6 @@
 "use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+ 
 import {
   Select,
   SelectContent,
@@ -11,6 +13,7 @@ import { getAnalytics } from "@/actions/analytics";
 import { useFetch } from "@/hooks/use-fetch";
 import { useUser } from "@clerk/nextjs";
 import MoodAnalyticsSkeleton from "./analytics-loading";
+import { getMoodById, getMoodTrend } from "@/app/lib/mood";
 const MoodAnalaysis = () => {
   const timeOption = [
     { value: "7d", label: "Last 7 days" },
@@ -28,11 +31,11 @@ const MoodAnalaysis = () => {
     fetchAnalytics(peroid);
   }, [peroid]);
 
-
-  console.log(analytics);
-if(loading||!isLoaded||!analytics?.data){
-return <MoodAnalyticsSkeleton/>
-}
+ 
+  if (loading || !isLoaded || !analytics?.data) {
+    return <MoodAnalyticsSkeleton />;
+  }
+  const {timeLine,stats}=analytics.data;
   return (
     <>
       <div className="flex justify-between items-center">
@@ -51,6 +54,50 @@ return <MoodAnalyticsSkeleton/>
             })}
           </SelectContent>
         </Select>
+      </div>
+      <div className="space-y6">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <Card >
+            <CardHeader className="pb-2">
+              <CardTitle className={"text-sm font-medium"}>
+                Total Entries
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="font-bold text-2xl">{stats.totalEntries}</p>
+              <p className="text-sm text-muted-foreground">
+
+                ~{stats.dailyAverage} entries per day</p>
+            </CardContent>
+          </Card>
+        <Card >
+            <CardHeader className="pb-2">
+              <CardTitle className={"text-sm font-medium"}>
+                Average Score
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="font-bold text-2xl">{stats.averageScore}/10</p>
+              <p className="text-sm text-muted-foreground">
+
+                ~overall mood Score</p>
+            </CardContent>
+          </Card>
+        <Card >
+            <CardHeader className="pb-2">
+              <CardTitle className={"text-sm font-medium"}>
+                Mood Summary
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+             <div className="">
+              {getMoodById(stats.mostFrequentMood)?.emoji}{' '}
+              {getMoodTrend(stats.averageScore)}
+             </div>
+            </CardContent>
+          </Card>
+        </div>
+        
       </div>
     </>
   );
